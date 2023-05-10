03a63da54e376aa33f059cca2a856e50a8723dbf
ivan <82367614+ivan3035789@users.noreply.github.com>
2023-05-10 22:33:46 +0300

    update

--- src/test/java/page/PageShop.java
+++ src/test/java/page/PageShop.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 public class PageShop {
-//    Pet pet = new Pet("89430780", "1", "cat", "crty", "1", "crty", "available");
 
     private String baseUri = "https://petstore.swagger.io/v2";
 
@@ -13,17 +12,14 @@ public class PageShop {
     public String petAddStore() {
         return "/store/order";
     }
-//    private String petAddStore = "/store/order";
 
     @Description("удаление питомца из заказа")
     public String petRemovalStore(String IdOfTheCreatedPet) {
         return "/store/order/" + IdOfTheCreatedPet;
     }
-//    private String petRemovalStore = "/store/order/" + pet.getIdOfTheCreatedPet();
 
     @Description("просмотр питомца в магазине")
     public String petGetStore(String getIdOfTheCreatedPet) {
         return "/store/order/" + getIdOfTheCreatedPet;
     }
-//    private String petGetStore = "/store/order/" + pet.getIdOfTheCreatedPet();
 }
--- src/test/java/page/PageUser.java
+++ src/test/java/page/PageUser.java
@@ -1,20 +1,10 @@
 package page;
 
-import data.User;
 import jdk.jfr.Description;
 import lombok.Data;
 
 @Data
 public class PageUser {
-    User user = new User(
-            "894307801",
-            "Genka",
-            "Genadiy",
-            "Ivanov",
-            "Genadiy@rambler.ru",
-            "0000",
-            "83332222222",
-            "1");
 
     private String baseUri = "https://petstore.swagger.io/v2";
 
@@ -22,28 +12,24 @@ public class PageUser {
     public String creationUser() {
         return "/user";
     }
-//    private String creationUser = "/user";
 
     @Description("удаление пользователя")
     public String removalUser(String Username) {
         return "/user/" + Username;
     }
-//    private String removalUser = "/user/" + user.getUsername();
 
     @Description("просмотр пользователя")
     public String userGetPage(String Username) {
         return "/user/" + Username;
     }
-//    private String userGetPage = "/user/" + user.getUsername();
+
     @Description("регистрирует пользователя в системе")
     public String logsUserIntoSystem(String Username, String Password) {
         return "/user/login?username=" + Username + "&password=" + Password;
     }
-//    private String logsUserIntoSystem = "/user/login?username=" + user.getUsername() + "&password=" + user.getPassword();
 
     @Description("выход пользователя из системы")
     public String userLogoutFromSystem(String Username) {
         return "/user/logout?username=" + Username;
     }
-//    private String userLogoutFromSystem = "/user/logout?username=" + user.getUsername();
 }
